960edad2cb7b833b2db3ab74b4933ed38769b03e
Kickness <caswallengelsman6@gmail.com>
2020-08-04 23:18:03 +0200

    Adding new configs

---
+++ etc/xinitrc.d/51-xrandr.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/sh
+xrandr --auto --output HDMI-0 --primary --mode 1920x1080 --right-of LVDS
+xrandr --output HDMI-0 --set TearFree on
---
+++ etc/xinitrc.d/52-background-image.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+while :
+do
+	feh --bg-fill --recursive --randomize $HOME/Pictures/wallpapers $HOME/Pictures/wallpapers
+	sleep 30
+done &
