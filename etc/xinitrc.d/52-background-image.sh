aa67b740cefef81aa4e38270722651bdedb1b51e
Caswall Engelsman <mail@cengelsman.com>
2022-07-23 22:06:58 +0200

    First version of polybar

--- etc/xinitrc.d/52-background-image.sh
+++ etc/xinitrc.d/52-background-image.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
-WALLPAPER_DIR=$HOME/Cloud/Pictures/wallpapers \
+pkill -f $0
+WALLPAPER_DIR=$HOME/Cloud/Pictures/wallpapers
 sh -c 'while :
 do
     feh --bg-fill --recursive --randomize $WALLPAPER_DIR $WALLPAPER_DIR
