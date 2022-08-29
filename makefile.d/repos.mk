1f315c2b264bd44f71b4a3d09d2fbdf44e97f969
Caswall Engelsman <mail@cengelsman.com>
2022-08-29 23:04:37 +0200

    Add repos make script

---
+++ makefile.d/repos.mk
@@ -0,0 +1,24 @@
+REPO_DIR := ${HOME}/Documents
+GITHUB_URL = git@github.com:Kick1911
+GITLAB_URL = git@gitlab.com:Kickness
+
+GITHUB_REPOS = python-tidal dotfiles WorkForce freeswitch-cluecon-lab Hash_Table
+GITLAB_REPOS = cpm json gaze kickc kick-display tcphttp unitest
+
+GITHUB_PATHS := ${GITHUB_REPOS:%=${REPO_DIR}/%}
+GITLAB_PATHS := ${GITLAB_REPOS:%=${REPO_DIR}/%}
+
+define clone
+	${call print,${CYAN}GITHUB:CLONE ${BRIGHT_CYAN}${notdir ${2}}}
+	${Q}git clone ${1}/${notdir ${2}}.git ${2}
+endef
+
+${GITHUB_PATHS}:
+	${call clone,${GITHUB_URL},$@}
+
+${GITLAB_PATHS}:
+	${call clone,${GITLAB_URL},$@}
+
+repos: ${GITLAB_PATHS} ${GITHUB_PATHS}
+
+.PHONY: repos
