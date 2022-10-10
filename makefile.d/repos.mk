b02f78d9a9fb29d3be930607d6477fd4f96b2b76
Caswall Engelsman <mail@cengelsman.com>
2022-10-10 21:28:20 +0200

    Add dlinked_list repo

--- makefile.d/repos.mk
+++ makefile.d/repos.mk
@@ -4,7 +4,7 @@ GITHUB_URL = git@github.com:Kick1911
 GITLAB_URL = git@gitlab.com:Kickness
 
 GITHUB_REPOS = python-tidal dotfiles WorkForce freeswitch-cluecon-lab Hash_Table
-GITLAB_REPOS = cpm json gaze kickc kick-display tcphttp unitest
+GITLAB_REPOS = cpm json gaze kickc kick-display tcphttp unitest dlinked_list
 
 GITHUB_PATHS := ${GITHUB_REPOS:%=${REPO_DIR}/%}
 GITLAB_PATHS := ${GITLAB_REPOS:%=${REPO_DIR}/%}
@@ -18,7 +18,7 @@ ${GITLAB_PATHS}:
 repos: ${GITLAB_PATHS} ${GITHUB_PATHS}
 
 define clone
-	${call print,${CYAN}GITHUB:CLONE ${BRIGHT_CYAN}${notdir ${2}}}
+	${call print,${CYAN}$1:CLONE ${BRIGHT_CYAN}${notdir ${2}}}
 	${call as_user, git clone ${1}/${notdir ${2}}.git ${2}}
 endef
 
