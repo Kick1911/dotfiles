71d72e1107c4be701a3c78e92b10f4c660b2ceba
Caswall Engelsman <mail@cengelsman.com>
2022-12-21 00:50:44 +0200

    Fix dynamic number line colours

--- nvim/lua/number_line.lua
+++ nvim/lua/number_line.lua
@@ -1,19 +1,37 @@
+require "functional"
+
 function numberBgColour()
     local r, c = unpack(vim.api.nvim_win_get_cursor(0))
     local total = vim.api.nvim_buf_line_count(0)
 
     ratio = r / total
-    if ratio < 0.2 then
+    cleaned_ratio = reduce(
+        function(a, b)
+            if math.floor(ratio / b) == 1 then
+                return b
+            elseif math.floor(ratio / a) == 1 then
+                return a
+            else
+                return 0
+            end
+        end,
+        {0.2, 0.4, 0.6, 0.8}
+    )
+
+    if cleaned_ratio == 0 then
         vim.cmd [[ hi CursorLineNr ctermfg=cyan ]]
-    elseif ratio < 0.4 and ratio > 0.2 then
-        vim.cmd [[ hi CursorLineNr ctermfg=green ]]
-    elseif ratio < 0.6 and ratio > 0.4 then
-        vim.cmd [[ hi CursorLineNr ctermfg=yellow ]]
-    elseif ratio < 0.8 and ratio > 0.6 then
-        vim.cmd [[ hi CursorLineNr ctermfg=magenta ]]
-    elseif ratio <= 1 and ratio > 0.8 then
-        vim.cmd [[ hi CursorLineNr ctermfg=red ]]
+        return
     end
+
+    local funcs = {
+        [0.2] = function() vim.cmd [[ hi CursorLineNr ctermfg=green ]] end,
+        [0.4] = function() vim.cmd [[ hi CursorLineNr ctermfg=yellow ]] end,
+        [0.6] = function() vim.cmd [[ hi CursorLineNr ctermfg=magenta ]] end,
+        [0.8] = function() vim.cmd [[ hi CursorLineNr ctermfg=red ]] end
+    }
+    print("cleaned_ratio", cleaned_ratio)
+    local func = funcs[cleaned_ratio]
+    if func then func() end
 end
 
 local _group = vim.api.nvim_create_augroup("LineNumber", { clear = true })
