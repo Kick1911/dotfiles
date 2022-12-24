fb73237ad6e3e760a0f16932f503b26f017bcbbd
Caswall Engelsman <mail@cengelsman.com>
2022-12-24 15:35:39 +0200

    My personal laptop changes

--- etc/xinitrc.d/52-background-image.sh
+++ etc/xinitrc.d/52-background-image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # pkill -f $0
-WALLPAPER_DIR=/data/Cloud/Pictures/wallpapers
+WALLPAPER_DIR=$HOME/Cloud/Pictures/wallpapers
 while :
 do
     feh --bg-fill --recursive --randomize $WALLPAPER_DIR $WALLPAPER_DIR
