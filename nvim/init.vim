642431b85a9e774299986e868f0d64cf4ad70c52
Caswall Engelsman <mail@cengelsman.com>
2023-04-19 12:40:03 +0200

    Add number_line

--- nvim/init.vim
+++ nvim/init.vim
@@ -1,3 +1,4 @@
+lua require('number_line')
 " Specify a directory for plugins
 " - For Neovim: ~/.local/share/nvim/plugged
 " - Avoid using standard Vim directory names like 'plugin'
---
+++ nvim/lua/number_line.lua
@@ -0,0 +1,46 @@
+require "ratio"
+
+function precision(f, digits)
+    return math.floor(f * (10 * digits)) / (10 * digits)
+end
+
+function numberBgColour()
+    local r, c = unpack(vim.api.nvim_win_get_cursor(0))
+    local total = vim.api.nvim_buf_line_count(0)
+
+    ratio = r / total
+
+    local cleaned_ratio = precision(clean_ratio(ratio), 1) -- C function
+    if cleaned_ratio == 0 then
+        vim.cmd [[ hi CursorLineNr ctermfg=cyan ]]
+        return
+    end
+
+    -- Colour ref: https://vimdoc.sourceforge.net/htmldoc/syntax.html#:highlight
+    local funcs = {
+        [0.2] = function() vim.cmd [[ hi CursorLineNr ctermfg=green ]] end,
+        [0.4] = function() vim.cmd [[ hi CursorLineNr ctermfg=yellow ]] end,
+        [0.6] = function() vim.cmd [[ hi CursorLineNr ctermfg=magenta ]] end,
+        [0.8] = function() vim.cmd [[ hi CursorLineNr ctermfg=red ]] end
+    }
+    -- print("cleaned_ratio", cleaned_ratio)
+    local func = funcs[cleaned_ratio]
+    if func then func() end
+end
+
+local _group = vim.api.nvim_create_augroup("LineNumber", { clear = true })
+
+vim.cmd [[
+set cursorline
+set cursorlineopt=number
+hi CursorLineNr cterm=bold
+]]
+vim.api.nvim_create_autocmd(
+    "CursorMoved",
+    {
+        pattern = "*",
+        callback = numberBgColour,
+        once = false,
+        group = _group
+    }
+)
