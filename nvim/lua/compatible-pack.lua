09523d876c7589f5a1046615b218b0e87b0c0b0a
Caswall Engelsman <mail@cengelsman.com>
2023-04-24 02:41:49 +0200

    I think I got the legacy vi undo behaviour

--- nvim/lua/compatible-pack.lua
+++ nvim/lua/compatible-pack.lua
@@ -1,6 +1,7 @@
 local state = "redo"
 
-function ChangeDirection()
+-- TODO: This needs to reset when new changes are made
+function ChangeDirection(ev)
     if state == "undo" then
         state = "redo"
     else
@@ -8,6 +9,7 @@ function ChangeDirection()
     end
     NextStep()
 end
+
 
 function NextStep()
     if state == "undo" then
@@ -17,5 +19,21 @@ function NextStep()
     end
 end
 
+
+function ResetState(ev)
+    -- print(vim.inspect(ev))
+    state = "redo"
+end
+
+
 vim.keymap.set({"n"}, "u", ChangeDirection)
 vim.keymap.set({"n"}, "<C-r>", NextStep)
+
+vim.api.nvim_create_autocmd(
+    { "InsertChange" },
+    {
+        pattern = "*",
+        callback = ResetState,
+        once = false,
+    }
+)
