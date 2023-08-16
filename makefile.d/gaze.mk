a7ff751553d4fd91fc40e420caf1db2a90de0e68
Caswall Engelsman <mail@cengelsman.com>
2023-08-17 00:43:27 +0200

    Use offical gaze install method

--- makefile.d/gaze.mk
+++ makefile.d/gaze.mk
@@ -4,8 +4,8 @@ GAZE_CONFIG_DIR = /etc/gaze
 
 gaze: ${GAZE_CONFIG_DIR} ${GAZE_BIN}
 
-${GAZE_BIN}: ${HOME}/Documents/gaze
-	cd $< && make && make install
+${GAZE_BIN}:
+	curl https://gitlab.com/api/v4/projects/38792435/packages/generic/gaze/v0.12.5/install.sh | sh
 
 /etc/gaze:
 	${call print,${CYAN}LN $@}
