4a656aa294cf9024cb21c22738ed69aa733941f9
Caswall Engelsman <mail@cengelsman.com>
2025-06-15 20:11:36 +0200

    Disable delete key as well

--- nvim/lua/plugins/backspace.lua
+++ nvim/lua/plugins/backspace.lua
@@ -2,7 +2,13 @@ local M = {}
 
 local insert_start_col = nil
 
-local function disable(key)
+local function disable_while_insert_mode(key)
+  vim.keymap.set("i", key, function()
+    return ""
+  end)
+end
+
+local function disable_non_buffer_insert_backspace(key)
   vim.keymap.set("i", key, function()
     local _, col = unpack(vim.api.nvim_win_get_cursor(0))
     if insert_start_col and col <= insert_start_col then
@@ -20,8 +26,9 @@ function M.setup()
     end
   })
 
-  disable("<BS>")
-  disable("<C-w>")
+  disable_while_insert_mode("<Del>")
+  disable_non_buffer_insert_backspace("<BS>")
+  disable_non_buffer_insert_backspace("<C-w>")
 end
 
 return M
